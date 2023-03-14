d2dcf9f3f3d534d82bfe4cf106ec8f508dd08aa2
le van hai <lvhai2k2@gmail.com>
2023-03-15 06:35:56 +0700

    add_home

--- src/features/main/home/screens/home/HomeScreenStyles.js
+++ src/features/main/home/screens/home/HomeScreenStyles.js
@@ -1,10 +1,16 @@
 import { StyleSheet } from 'react-native'
-
+import app_sp from 'styles/spacing';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+  }, 
+  homeTitleHeader: {
+    fontSize: 32, 
+    lineHeight: 39, 
+    marginBottom: 61,
+    ...app_sp.mh_18,
+    ...app_sp.mt_12,
   }
 });
 
